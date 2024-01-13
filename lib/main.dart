931fb92104275d085aaa489b677c940e3fe90eaf
GwonDooHyeon <secure3141@gmail.com>
2024-01-14 03:05:28 +0900

    기본 UI 작업

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:delivery/common/component/custom_text_form_field.dart';
+import 'package:delivery/user/view/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const _App());
+}
+
+class _App extends StatelessWidget {
+  const _App();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'NotoSans'),
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    );
+  }
+}
